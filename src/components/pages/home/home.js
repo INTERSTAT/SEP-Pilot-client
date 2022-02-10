53069d88e19d03192b6ba9b83bbd7687108b2582
Francesca D'Agresti <Francesca.DAgresti@eng.it>
2022-02-10 19:20:51 +0100

    Homepage updates

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import './home.css';
 
-const HomePage = () => <div>Add interstat logo</div>;
+const HomePage = () => ( 
+	<div className="Homepage">
+        <img src="https://cef-interstat.eu/wp-content/uploads/2020/12/INTERSTAT_Symbol.png" className="App-logo" alt="logo" />
+        <p>
+          INTERSTAT Pilots Application
+        </p>
+        <a
+          className="Framework-link"
+          href="https://interstat.eng.it/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Framework homepage
+        </a>
+	</div>
+);
 
 export default HomePage;
