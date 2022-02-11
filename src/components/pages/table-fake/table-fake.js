7f81ee746c9e1808c13450dc845a7139bce079f1
NicoLaval <nicolas.laval@insee.fr>
2022-02-11 13:01:46 +0100

    Add Accordion component

--- src/components/pages/table-fake/table-fake.js
+++ src/components/pages/table-fake/table-fake.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
 import DataTable from 'react-data-table-component';
+import Accordion from 'components/common/accordion';
 import Paper from 'components/common/paper';
 import { FAKE_COLUMNS, FAKE_DATA, FAKE_TEXT } from './fake-data';
 
 const TableFakePage = () => (
 	<Grid container spacing={2}>
 		<Grid item xs={4}>
+			<div style={{ marginBottom: '1em' }}>
+				<Accordion title="Accordion sample">{FAKE_TEXT}</Accordion>
+			</div>
 			<Paper>{FAKE_TEXT}</Paper>
 		</Grid>
 		<Grid item xs={8}>
