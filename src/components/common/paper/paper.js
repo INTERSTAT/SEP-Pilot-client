d4f7dcd205b2b480d2856a29edfb34c61699cd16
NicoLaval <nicolas.laval@insee.fr>
2022-02-11 13:01:56 +0100

    Improve paper component

--- src/components/common/paper/paper.js
+++ src/components/common/paper/paper.js
@@ -1,10 +1,14 @@
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 
-const PaperInterstat = styled(Paper)(({ theme }) => ({
+const StyledPaperInterstat = styled(Paper)(({ theme }) => ({
 	...theme.typography.body2,
 	padding: theme.spacing(1),
 	color: theme.palette.text.secondary,
 }));
+
+const PaperInterstat = ({ children }) => (
+	<StyledPaperInterstat elevation={8}>{children}</StyledPaperInterstat>
+);
 
 export default PaperInterstat;
